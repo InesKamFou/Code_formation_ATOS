5f369a29b79ce20bc0b32bab0cf0b05345bb75c7
InesKamFou <ineskammoun@isimsf.u-sfax.tn>
2026-02-20 16:06:08 +0100

    to_json

--- scripts/csv_pandas.py
+++ scripts/csv_pandas.py
@@ -1,15 +1,17 @@
 import pandas as pd # pd : alias
 
-df = pd.read_csv("../input/personnes.csv")
-print(df)
+df = pd.read_csv("input/personnes.csv")
+# print(df)
 # Les noms
 
 df_nom = df['Nom']
-print(df_nom)
+#print(df_nom)
 
 # Filter les personnes par pays
 df_france = df[(df['Pays']=='France') & (df['Sexe']=='F')]
-print(df_france)
+#print(df_france)
 print(len(df_france))
-df_france.to_csv("../output/Personnes_françaises.csv", sep= ';', index = False )
+df_france.to_csv("output/Personnes_françaises.csv", sep= ';', index = False )
+
+df_france.to_json("output/peronnes_françaises.json",orient="records",force_ascii=False, indent=2 )
 
