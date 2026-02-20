7baf1d4ff96c837210e65072ab5066594ab3a9e2
InesKamFou <ineskammoun@isimsf.u-sfax.tn>
2026-02-20 09:59:11 +0100

    json_csv

---
+++ csv_pandas.py
@@ -0,0 +1,15 @@
+import pandas as pd # pd : alias
+
+df = pd.read_csv("personnes.csv")
+print(df)
+# Les noms
+
+df_nom = df['Nom']
+print(df_nom)
+
+# Filter les personnes par pays
+df_france = df[(df['Pays']=='France') & (df['Sexe']=='F')]
+print(df_france)
+print(len(df_france))
+df_france.to_csv("Personnes_françaises.csv", sep= ';', index = False )
+
---
+++ json_handling.py
@@ -0,0 +1,29 @@
+import json
+donnees_initiales = {
+    "entreprise": "TechNova",
+    "employes": [
+        {
+            "id": 1,
+            "nom": "Alice",
+            "poste": "Développeuse",
+            "competences": ["Python", "SQL"]
+        },
+        {
+            "id": 2,
+            "nom": "Martin",
+            "poste": "Analyste",
+            "competences": ["Excel", "Power BI"]
+        }
+    ],
+    "localisation": {
+        "pays": "France",
+        "ville": "Lyon"
+    }
+}
+
+with open("donnees.json", "w", encoding="utf-8") as f:
+    json.dump(donnees_initiales, f, indent=4, ensure_ascii=False)
+
+with open('donnees.json', mode = 'r', encoding = 'utf-8') as f:
+    data = json.load(f)
+print(data)
